34780e53c4c7b9f210824d2cdc95b8ab6d79ff4b
Odion Sonny-Egbeahie <99491084+Odion-Sonny@users.noreply.github.com>
2023-05-15 08:38:06 +0100

    Update C.h

--- C.h
+++ C.h
@@ -154,3 +154,192 @@ int main() {
 
 }
 
+
+int main() {
+
+    int num1 = 10;
+
+    int num2 = 5;
+
+    int sum = num1 + num2;
+
+    printf("The sum is: %d\n", sum);
+
+    float pi = 3.14159;
+
+    printf("The value of pi is: %.2f\n", pi);
+
+    char letter = 'A';
+
+    printf("The letter is: %c\n", letter);
+
+    int i;
+
+    for (i = 0; i < 10; i++) {
+
+        printf("%d ", i);
+
+    }
+
+    printf("\n");
+
+    int num = 7;
+
+    if (num % 2 == 0) {
+
+        printf("%d is even\n", num);
+
+    } else {
+
+        printf("%d is odd\n", num);
+
+    }
+
+    int x = 5;
+
+    int y = 2;
+
+    int result = x / y;
+
+    printf("Result: %d\n", result);
+
+    int array[5] = {1, 2, 3, 4, 5};
+
+    for (i = 0; i < 5; i++) {
+
+        printf("%d ", array[i]);
+
+    }
+
+    printf("\n");
+
+    int factorial = 1;
+
+    int n = 5;
+
+    for (i = 1; i <= n; i++) {
+
+        factorial *= i;
+
+    }
+
+    printf("Factorial of %d is: %d\n", n, factorial);
+
+    int a = 10;
+
+    int b = 20;
+
+    int temp;
+
+    temp = a;
+
+    a = b;
+
+    b = temp;
+
+    printf("a = %d, b = %d\n", a, b);
+
+    int n1 = 10;
+
+    int n2 = 20;
+
+    int max = (n1 > n2) ? n1 : n2;
+
+    printf("The maximum number is: %d\n", max);
+
+    int number = 10;
+
+    int sum = 0;
+
+    while (number > 0) {
+
+        sum += number;
+
+        number--;
+
+    }
+
+    printf("Sum: %d\n", sum);
+
+    int marks = 85;
+
+    if (marks >= 90) {
+
+        printf("Grade: A\n");
+
+    } else if (marks >= 80) {
+
+        printf("Grade: B\n");
+
+    } else if (marks >= 70) {
+
+        printf("Grade: C\n");
+
+    } else if (marks >= 60) {
+
+        printf("Grade: D\n");
+
+    } else {
+
+        printf("Grade: F\n");
+
+    }
+
+    int age = 25;
+
+    switch (age) {
+
+        case 18:
+
+            printf("You are 18 years old\n");
+
+            break;
+
+        case 25:
+
+            printf("You are 25 years old\n");
+
+            break;
+
+        default:
+
+            printf("Your age is neither 18 nor 25\n");
+
+    }
+
+    int num = 10;
+
+    do {
+
+        printf("%d ", num);
+
+        num--;
+
+    } while (num > 0);
+
+    printf("\n");
+
+    int base = 2;
+
+    int exponent = 3;
+
+    int result = 1;
+
+    for (i = 0; i < exponent; i++) {
+
+        result *= base;
+
+    }
+
+    printf("Result: %d\n", result);
+
+    int num = 1234;
+
+    int reverse = 0;
+
+    while (num > 0) {
+
+        reverse = reverse * 10 + num % 10;
+
+        num /= 10
+
